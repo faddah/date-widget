2a1387263dcf11c35f1a8e8023831897b771b594
Faddah Wolf <my_biz@me.com>
2025-01-08 18:45:30 -0800

    Created files — index.hml, styles.css & date.js — and populated them with code from this tutorial: https://www.youtube.com/watch?v=kn1bNLMTgdo.

---
+++ date.js
@@ -0,0 +1,22 @@
+/*
+Taken from Youtube tutorial: https://www.youtube.com/watch?v=kn1bNLMTgdo
+		Making a Calendar in Vanilla Javascript | Design a Simple Calendar for Your Blog and Website
+		by Online Tutorials
+*/
+
+console.log('date.js loaded');
+console.log('this is where the date should go...');
+
+const lang = navigator.language;
+let date = new Date();
+
+let dayNumber = date.getDate();
+let month = date.getMonth();
+let dayName = date.toLocaleString(lang, { weekday: 'long' });
+let monthName = date.toLocaleString(lang, { month: 'long' });
+let year = date.getFullYear();
+
+document.querySelector('.monthName').innerHTML = monthName;
+document.querySelector('.dayName').innerHTML = dayName;
+document.querySelector('.dayNumber').innerHTML = dayNumber;
+document.querySelector('.year').innerHTML = year;
