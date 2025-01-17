eea2a5f078843d95dd21128e2f39959336bcdea9
Faddah Wolf <my_biz@me.com>
2025-01-17 14:51:45 -0800

    Added code to make sure time shows immeditately instead of 1/2 second of "00" at first.

--- date.js
+++ date.js
@@ -25,6 +25,7 @@ const hrs = document.querySelector(`#hrs`);
 const min = document.querySelector(`#min`);
 const sec = document.querySelector(`#sec`);
 
+/* * * * * * * * * * * * * * * * * * * * 
 setInterval(() => {
     let currentTime = new Date();
 
@@ -32,3 +33,18 @@ setInterval(() => {
     min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
+ * * * * * * * * * * * * * * * * * * * */
+
+const updateTime = () => {
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}
+
+// Call updateTime once to set the initial time
+updateTime();
+
+// Set interval to update the time every second
+setInterval(updateTime, 1000);
