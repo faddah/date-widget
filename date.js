61b1b92043aaf4a43b0ce3684fec64262f1b7637
Faddah Wolf <my_biz@me.com>
2025-01-21 20:01:40 -0800

    Refactored correction to the updateTime() code for when hours might come out negative.

--- date.js
+++ date.js
@@ -36,11 +36,12 @@ setInterval(() => {
  * * * * * * * * * * * * * * * * * * * */
 
 const updateTime = () => {
-    let currentTime = new Date();
-
-    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+  let currentTime = Math.floor(Date.now() / 1000);
+  hrs.innerHTML = Math.floor(((currentTime / 3600) % 24) <= 0 
+    ? (((currentTime / 3600) % 24) + 16).toString().padStart(2, '0')
+    : (((currentTime / 3600) % 24) - 8).toString().padStart(2, '0'));
+  min.innerHTML = Math.floor((currentTime / 60) % 60).toString().padStart(2, '0');
+  sec.innerHTML = (currentTime % 60).toString().padStart(2, '0');
 }
 
 // Call updateTime once to set the initial time
